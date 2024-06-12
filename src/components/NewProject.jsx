bd980a7e1afabc6d0995d960b21368044f8ba2aa
mohamuhsin <muhsinohoho@outlook.com>
2024-06-12 23:26:58 +0300

    styled our components

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -2,13 +2,17 @@ import Input from "./Input";
 
 export default function NewProject() {
     return (
-        <div>
-            <menu>
+        <div className="w-[35rem] mt-16">
+            <menu className="flex items-center justify-end gap-4 my-4">
                 <li>
-                    <button>Cancel</button>
+                    <button className="text-stone-800 hover:text-stone-950">
+                        Cancel
+                    </button>
                 </li>
                 <li>
-                    <button>Save</button>
+                    <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-900">
+                        Save
+                    </button>
                 </li>
             </menu>
             <div>
