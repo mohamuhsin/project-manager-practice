bf99d841ac1e3f30b75a6b9a1c16d97c5fd99e74
mohamuhsin <muhsinohoho@outlook.com>
2024-06-13 23:24:03 +0300

    created components and rendered them

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSideBar from "./components/ProjectsSideBar";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSideBar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
