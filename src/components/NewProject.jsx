1cee7eefd6f36df58758552f98b67c5134ab2ee9
mohamuhsin <muhsinohoho@outlook.com>
2024-06-13 23:45:00 +0300

    used refs to get input values

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,6 +1,18 @@
+import { useRef } from "react";
+
 import Input from "./Input";
 
 export default function NewProject() {
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+
+    function handleSave() {
+        const enteredTitle = title.current.value;
+        const enteredDescription = description.current.value;
+        const enteredDueDate = dueDate.current.value;
+    }
+
     return (
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
@@ -10,15 +22,18 @@ export default function NewProject() {
                     </button>
                 </li>
                 <li>
-                    <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-900">
+                    <button
+                        className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-900"
+                        onClick={handleSave}
+                    >
                         Save
                     </button>
                 </li>
             </menu>
             <div>
-                <Input label="Title" />
-                <Input label="Description" textarea />
-                <Input label="Due Date" />
+                <Input label="Title" ref={title} />
+                <Input label="Description" textarea ref={description} />
+                <Input label="Due Date" ref={dueDate} />
             </div>
         </div>
     );
