673059657a1c5c7a13f839cbb1c3cac8efc27a96
mohamuhsin <muhsinohoho@outlook.com>
2024-07-03 23:01:22 +0300

    we forwaded project data to sidebar component and styled them

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,7 @@ function App() {
 
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
@@ -44,7 +45,7 @@ function App() {
   }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSideBar onStartAddProject={handleStartAddProject} />
+      <ProjectsSideBar onStartAddProject={handleStartAddProject} projects={projectState.projects} />
       {content}
     </main>
   );
