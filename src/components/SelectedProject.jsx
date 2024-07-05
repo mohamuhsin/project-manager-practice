069362e4dc58237a9148bb031456a4b222b5b798
mohamuhsin <muhsinohoho@outlook.com>
2024-07-05 23:31:22 +0300

    added tasks and task component

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,3 +1,5 @@
+import Tasks from "./Tasks";
+
 export default function SelectedProject({ project, onDelete }) {
     const formattedDate = new Date(project.dueDate).toLocaleDateString("en-US", {
         year: "numeric",
@@ -23,7 +25,7 @@ export default function SelectedProject({ project, onDelete }) {
                     {project.description}
                 </p>
             </header>
-            TASKS
+            <Tasks />
         </div>
     );
 }
